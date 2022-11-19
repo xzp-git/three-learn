775e63b84c441cca346267fb0fa9b00939ed2e3d
roubaozi <xzp0319gr@163.com>
2022-11-19 23:22:08 +0800

    feat: three-basic

---
+++ src/createScene/App.tsx
@@ -0,0 +1,46 @@
+import "./App.css";
+import * as THREE from "three";
+import { useEffect } from "react";
+
+function renderBox() {
+  const scene = new THREE.Scene();
+
+  const camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+  );
+
+  camera.position.set(0, 0, 10);
+
+  scene.add(camera);
+
+  const cubeGeometry = new THREE.BoxGeometry(1, 1, 1);
+
+  const cubeMaterial = new THREE.MeshBasicMaterial({ color: 0xffff00 });
+
+  const cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
+
+  scene.add(cube);
+
+  const renderer = new THREE.WebGLRenderer();
+
+  renderer.setSize(window.innerWidth, window.innerHeight);
+
+  document.getElementById("three-app")?.appendChild(renderer.domElement);
+
+  renderer.render(scene, camera);
+}
+
+function App() {
+  let isExist = false;
+  useEffect(() => {
+    if (isExist) return;
+    isExist = true;
+    renderBox();
+  });
+  return <div id="three-app"></div>;
+}
+
+export default App;
---
+++ src/useControls/App.tsx
@@ -0,0 +1,78 @@
+import "./App.css";
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import { useEffect } from "react";
+
+function renderBox() {
+  // 创建场景
+  const scene = new THREE.Scene();
+
+  // 创建相机
+  const camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+  );
+
+  // 设置相机的位置
+  camera.position.set(0, 0, 10);
+
+  // 将相机添加到场景中
+  scene.add(camera);
+
+  // 创建几何体
+  const cubeGeometry = new THREE.BoxGeometry(1, 1, 1);
+
+  // 创建材质
+  const cubeMaterial = new THREE.MeshBasicMaterial({ color: 0xffff00 });
+
+  // 用几何体 和 材质 创建物体
+  const cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
+
+  // 修改物体的位置
+  // cube.position.set(5, 0, 0);
+  // cube.position.x = 4;
+
+  // 将物体添加到场景
+  scene.add(cube);
+
+  // 创建渲染器
+  const renderer = new THREE.WebGLRenderer();
+
+  // 设置渲染器的大小
+  renderer.setSize(window.innerWidth, window.innerHeight);
+
+  //将渲染元素插入页面
+  document.getElementById("three-app")?.appendChild(renderer.domElement);
+
+  // 创建控制器 控制相机的移动
+  new OrbitControls(camera, renderer.domElement);
+
+  // 设置坐标轴
+  const axesHelper = new THREE.AxesHelper(5);
+
+  // 将坐标轴添加到场景
+  scene.add(axesHelper);
+
+  function render() {
+    cube.position.x += 0.01;
+    if (cube.position.x > 5) {
+      cube.position.x = 0;
+    }
+
+    // 开启渲染
+    renderer.render(scene, camera);
+    requestAnimationFrame(render);
+  }
+  render();
+}
+
+function App() {
+  useEffect(() => {
+    renderBox();
+  });
+  return <div id="three-app"></div>;
+}
+
+export default App;
